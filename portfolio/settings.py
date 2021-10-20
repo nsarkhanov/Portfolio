06012aef6b2227d7c769fb75876d6adc1f10d5bc
nsarkhanov <nsarkhanov@gmail.com>
2021-10-21 01:32:39 +0200

    disable email

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,11 +139,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 
 # Activate Django-Heroku.
